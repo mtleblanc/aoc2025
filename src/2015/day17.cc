dbc1cbfad563993a0a8007950b84aa4b9d5eacc9
Michael LeBlanc <leblanc.michael@gmail.com>
2026-01-26 16:20:04 -0500

    2015 Day 17

--- src/2015/day17.cc
+++ src/2015/day17.cc
@@ -1,4 +1,5 @@
 #include "aoc.hh"
+#include <span>
 
 /* https://adventofcode.com/2015/day/17
  */
@@ -7,9 +8,48 @@ namespace aoc
 constexpr size_t YEAR = 2015;
 constexpr size_t DAY = 17;
 
+namespace
+{
+
+size_t part1(std::span<size_t> containers, size_t target)
+{
+    if (containers.empty())
+    {
+        return target == 0 ? 1 : 0;
+    }
+    auto next = containers.subspan(1);
+    return part1(next, target) +
+           (containers.front() > target ? 0 : part1(next, target - containers.front()));
+}
+
+// We could do this in one pass and count how many containers are used for each match, but input
+// size is small enough that just trying n=1,2,.. runs in 3.9ms
+size_t part2(std::span<size_t> containers, size_t target, size_t n)
+{
+    if (containers.empty() || n == 0)
+    {
+        return target == 0 ? 1 : 0;
+    }
+    auto next = containers.subspan(1);
+    return part2(next, target, n) +
+           (containers.front() > target ? 0 : part2(next, target - containers.front(), n - 1));
+}
+} // namespace
 template <> Solution solve<YEAR, DAY>(std::istream& input)
 {
-    (void)input;
-    return {};
+    constexpr size_t TARGET_SIZE = 150;
+    std::vector<size_t> containers;
+    for (size_t sz{}; input >> sz;)
+    {
+        containers.push_back(sz);
+    }
+
+    size_t p2{};
+    for (size_t n{1}; p2 == 0; ++n)
+    {
+        p2 = part2(containers, TARGET_SIZE, n);
+    }
+
+    return {part1(containers, TARGET_SIZE), p2};
 }
 } // namespace aoc
